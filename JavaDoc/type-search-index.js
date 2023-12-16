a8e4c7d477b2b7cc75e5d946920a698a42e34c87
Анастасия Золотарёва <miss.asyaZ@mail.ru>
2023-12-16 22:34:09 +0300

    javadoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Data"},{"p":"org.example","l":"Dijkstra"},{"p":"org.example","l":"Distances"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Print"}];updateSearchResults();
